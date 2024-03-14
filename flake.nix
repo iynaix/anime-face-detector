f8d9ffaca7b344cb578c803824725ed602501e40
Lin Xianyi <iynaix@gmail.com>
2024-03-14 11:07:34 +0800

    Use flake-parts template for devenv instead

--- flake.nix
+++ flake.nix
@@ -5,67 +5,60 @@
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
   };
-
   outputs =
-    {
-      nixpkgs,
-      devenv,
-      systems,
-      ...
-    }@inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      devShells = forEachSystem (
-        system:
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devenv.flakeModule ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
         let
-          pkgs = import nixpkgs { inherit system; };
+          ps = pkgs.python3Packages;
+          # python dependencies locked to the specific nixpkgs
+          mmcv-patched = ps.callPackage ./nix/mmcv { };
+          mmdet = (ps.callPackage ./nix/mmdet { mmcv = mmcv-patched; });
+          mmpose = (
+            ps.callPackage ./nix/mmpose {
+              mmcv = mmcv-patched;
+              xtcocotools = ps.callPackage ./nix/xtcocotools { };
+            }
+          );
         in
         {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [
-              {
-                # https://devenv.sh/reference/options/
-                dotenv.disableHint = true;
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+          devenv.shells.default = {
+            # https://devenv.sh/reference/options/
+            dotenv.disableHint = true;
 
-                # python
-                languages.python = {
-                  enable = true;
-                  # provide hard to compile packages to pip
-                  package = pkgs.python3.withPackages (
-                    ps:
-                    let
-                      mmcv-patched = ps.callPackage ./nix/mmcv { };
-                    in
-                    with ps;
-                    [
-                      mmcv-patched
-                      (ps.callPackage ./nix/mmdet { mmcv = mmcv-patched; })
-                      (ps.callPackage ./nix/mmpose {
-                        mmcv = mmcv-patched;
-                        xtcocotools = ps.callPackage ./nix/xtcocotools { };
-                      })
-                      numpy
-                      pillow
-                      flake8
-                      black
-                    ]
-                  );
-                };
-              }
-            ];
+            # python
+            languages.python = {
+              enable = true;
+              # provide hard to compile packages to pip
+              package = pkgs.python3.withPackages (
+                ps: with ps; [
+                  mmcv-patched
+                  mmdet
+                  mmpose
+                  numpy
+                  pillow
+                  flake8
+                  black
+                ]
+              );
+            };
           };
-        }
-      );
 
-      packages = forEachSystem (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        { }
-      );
+          packages = { };
+        };
     };
 }
